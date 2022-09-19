0b407f14d2159a2bc34ba96f592f353bd6d053a3
_Dewelg_ <_(dewel713@gmail.com_)>
2022-09-19 10:53:41 -0400

    done

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-const firstName = Dewel;
-let lastName = 'Gonzalez';
-var age = 18;
+//this is just to console.log all the varibles makes it faster
+function variables() {
+  const firstName = 'Dewel';
+  let lastName = 'Gonzalez';
+  var age = 18;
+  
+  let language = "JavaScript";
+  let createdYear = 1995;
+  let isCaseSentsitive = true;
+  
+  let price = 19.99;
+  let isonsale = false;
+  let salePercentage = 15;
+  let stock = 0;
+  let inStock = false;
+  let selectSize = 'M';
+  
+  let title = 'Name of the Wind';
+  let author = 'Patrick Rothfuss';
+  let pageCount = 722;
+  let bookmark = 456;
+  let hasRead = true;
+}
 
-let language = "JavaScript";
-let createdYear = 1995;
-let isCaseSentsitive = true;
-
-let price = 19.99;
-let isonsale = false;
-let salePercentage = 15;
-let stock = 0;
-let inStock = false;
-let selectSize = 'M';
-
-let title = 'Name of the Wind';
-let author = 'Patrick Rothfuss';
-let pageCount = 722;
-let bookmark = 456;
-let hasRead = true;
